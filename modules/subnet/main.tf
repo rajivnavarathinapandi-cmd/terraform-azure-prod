23eb17b1271201d258f38f57fb2db7569c363eb6
Ubuntu <azureuser@VM-devops.iwgh0bjloakubk02irossnxcdd.ax.internal.cloudapp.net>
2026-03-10 04:38:03 +0000

    Fix subnet NSG association logic

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -5,7 +5,8 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = var.address_prefixes
 }
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  count                     = var.nsg_id != "" ? 1 : 0
+  for_each = var.nsg_id != null ? { assoc = var.nsg_id } : {}
+
   subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = var.nsg_id
+  network_security_group_id = each.value
 }
