0bda223822046836e60b3e48e572791c4eb0a480
Ubuntu <azureuser@VM-devops.iwgh0bjloakubk02irossnxcdd.ax.internal.cloudapp.net>
2026-03-10 05:52:30 +0000

    clean dependency graph

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "resource_group" {
   source = "./modules/resource-group"
 
-  resource_group_name = "rg-terraform-demo" 
+  resource_group_name = "rg-terraform-demo"
   location            = "westeurope"
 }
 
@@ -13,12 +13,21 @@ module "vnet" {
   location            = "westeurope"
   resource_group_name = module.resource_group.resource_group_name
 }
+
+module "nsg_web" {
+  source = "./modules/nsg"
+
+  nsg_name            = "nsg-web"
+  location            = "westeurope"
+  resource_group_name = module.resource_group.resource_group_name
+}
+
 module "subnet_web" {
   source = "./modules/subnet"
 
   subnet_name          = "snet-web"
-  resource_group_name = module.resource_group.resource_group_name
-  virtual_network_name = "vnet-terraform-demo"
+  resource_group_name  = module.resource_group.resource_group_name
+  virtual_network_name = module.vnet.vnet_name
   address_prefixes     = ["10.20.1.0/24"]
 
   nsg_id = module.nsg_web.nsg_id
@@ -28,41 +37,32 @@ module "subnet_app" {
   source = "./modules/subnet"
 
   subnet_name          = "snet-app"
-  resource_group_name = module.resource_group.resource_group_name
-  virtual_network_name = "vnet-terraform-demo"
+  resource_group_name  = module.resource_group.resource_group_name
+  virtual_network_name = module.vnet.vnet_name
   address_prefixes     = ["10.20.2.0/24"]
+
   nsg_id = module.nsg_web.nsg_id
 }
-module "nsg_web" {
-  source = "./modules/nsg"
 
-  nsg_name            = "nsg-web"
-  location            = "westeurope"
-  resource_group_name = module.resource_group.resource_group_name
-}
 module "loadbalancer" {
-
   source = "./modules/loadbalancer"
 
   location            = "westeurope"
   resource_group_name = module.resource_group.resource_group_name
-
 }
 
 module "vm_web" {
-
   source = "./modules/vm"
 
-  vm_name = "vm-web"
+  vm_name  = "vm-web"
   vm_count = 2
   location = "westeurope"
 
   resource_group_name = module.resource_group.resource_group_name
-  subnet_id = module.subnet_web.subnet_id
+  subnet_id           = module.subnet_web.subnet_id
 
   backend_pool_id = module.loadbalancer.backend_pool_id
 
   admin_username = "azureuser"
   admin_password = "Password1234!"
-
 }
